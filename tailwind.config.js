b155aadc91930f1b392ca1899ec08e82345c4c4b
MUrilosilvalem <MURILOSILVALEM@GMAIL.COM>
2025-11-24 12:16:48 -0300

    feat: Integrate Tailwind CSS animations and utilities
    Adds dependencies for `tailwindcss-animate`, `clsx`, and `tailwind-merge` to enable advanced styling and animations. Configures Tailwind CSS to use `tailwindcss-animate` plugin.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindAnimate from 'tailwindcss-animate';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -18,5 +20,7 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    tailwindAnimate
+  ],
 }
