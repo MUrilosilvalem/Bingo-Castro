1f21a6af1b6ec4891fc4503b2c5f0b17128ab135
MUrilosilvalem <MURILOSILVALEM@GMAIL.COM>
2025-12-03 16:01:19 -0300

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ import tailwindAnimate from 'tailwindcss-animate';
 export default {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
